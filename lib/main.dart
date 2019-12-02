f93c9a6bf26f127f3d09387a2a337d71038edda5
Matt Juszczyk <matt.juszczyk@cqlcorp.com>
2019-12-02 15:59:16 -0500

    implement simple dropdown button that I can build off of later

--- lib/main.dart
+++ lib/main.dart
@@ -66,7 +66,7 @@ class _MyHomePageState extends State<MyHomePage> {
   String apiKey = '';
   String spotifyClientId = '';
   String spotifyClientSecret = '';
-
+  String dropdownValue = 'One';
   @override
   void initState() {
     super.initState();
@@ -278,6 +278,33 @@ class _MyHomePageState extends State<MyHomePage> {
                   fontWeight: FontWeight.bold,
                 ),
               ),
+            ),
+            DropdownButton<String>(
+              value: dropdownValue,
+              icon: Icon(Icons.arrow_downward),
+              iconSize: 24,
+              elevation: 16,
+              style: TextStyle(color: Colors.deepPurple),
+              underline: Container(
+                height: 2,
+                color: Colors.deepPurpleAccent,
+              ),
+              onChanged: (String newValue) {
+                setState(() {
+                  dropdownValue = newValue;
+                });
+              },
+              items: <String>[
+                'One',
+                'Two',
+                'Free',
+                'Four',
+              ].map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
             ),
           ],
         );
